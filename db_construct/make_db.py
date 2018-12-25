fbf5167a8e494be30347c99d2b8d83719363e2d0
valeria <leramorozova@github.com>
2018-12-25 13:20:24 +0200

    full db

--- db_construct/make_db.py
+++ db_construct/make_db.py
@@ -1,18 +1,12 @@
-from db_utils import Database
 import sqlite3
 import csv
 
-db_greek = Database("greek")
-db_polish = Database("polish")
 
-
-def makefile(lang):
-    if lang == "greek":
-        db = db_greek
-    else:
-        db = db_polish
+def makefile():
+    conn = sqlite3.connect('GrPolDict.db')
+    db = conn.cursor()
     try:
-        db.execute("DROP TABLE IF EXISTS simple_fields", 0)
+        db.execute("DROP TABLE IF EXISTS simple_fields")
         db.execute("""
                 CREATE TABLE simple_fields
                 (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -25,9 +19,10 @@ def makefile(lang):
                 translated_srcs TEXT NOT NULL,
                 rewrit_date TEXT NOT NULL,
                 orig_creation TEXT NOT NULL,
-                theme TEXT NOT NULL);
-                """, 0)
-        db.execute("DROP TABLE IF EXISTS complex_fields", 0)
+                theme TEXT NOT NULL,
+                lang TEXT NOT NULL);
+                """)
+        db.execute("DROP TABLE IF EXISTS complex_fields")
         db.execute("""
                     CREATE TABLE complex_fields
                     (lemma TEXT NOT NULL,
@@ -35,22 +30,27 @@ def makefile(lang):
                     semantics TEXT NOT NULL,
                     slavic_dict TEXT NOT NULL,
                     orig_srcs TEXT NOT NULL,
-                    citation TEXT NOT NULL);
-                    """, 0)
-        db.execute("DROP TABLE IF EXISTS sources", 0)
+                    citation TEXT NOT NULL,
+                    add_philol TEXT NOT NULL,
+                    add_histor TEXT NOT NULL,
+                    lang TEXT NOT NULL);
+                    """)
+        db.execute("DROP TABLE IF EXISTS sources")
         db.execute("""
                 CREATE TABLE sources
                 (lemma TEXT NOT NULL,
-                source TEXT NOT NULL
-                type TEXT NOT NULL);
-                """, 0)
-        db.execute("DROP TABLE IF EXISTS variants", 0)
+                source TEXT NOT NULL,
+                type TEXT NOT NULL,
+                lang TEXT NOT NULL);
+                """)
+        db.execute("DROP TABLE IF EXISTS variants")
         db.execute("""
                     CREATE TABLE variants
                     (lemma TEXT NOT NULL,
                     var_type TEXT NOT NULL,
-                    variant TEXT NOT NULL);
-                    """, 0)
+                    variant TEXT NOT NULL,
+                    lang TEXT NOT NULL);
+                    """)
         print("Tables has been created successfully")
     except Exception as e:
         print("Creation of tables failed in makefile.make_greek:\n")
@@ -66,7 +66,7 @@ def data_generator(lang):
 
 def fill_simple_fields(lang):
     print("filling simple data...")
-    conn = sqlite3.connect(lang + '_dict.db')
+    conn = sqlite3.connect('GrPolDict.db')
     db = conn.cursor()
     for row in data_generator(lang):
         lemma = row[0]
@@ -82,17 +82,17 @@ def fill_simple_fields(lang):
         db.execute('''
                 INSERT INTO simple_fields
                 (lemma_rus, pos, lemma_greek, if_hapax, if_middle_lang,
-                vasmer, translated_srcs, rewrit_date, orig_creation, theme)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                vasmer, translated_srcs, rewrit_date, orig_creation, theme, lang)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 ''', (lemma, pos, greek_lemma, if_hapax, if_middle_lang,
-                      vasmer, translated_srcs, rewrit_date, orig_creation, theme))
+                      vasmer, translated_srcs, rewrit_date, orig_creation, theme, lang))
         conn.commit()
     print("simple data has been filled successfully!")
 
 
 def fill_complex_fields(lang):
     print("filling complex data...")
-    conn = sqlite3.connect(lang + '_dict.db')
+    conn = sqlite3.connect('GrPolDict.db')
     db = conn.cursor()
     for row in data_generator(lang):
         lemma = row[0]
@@ -101,18 +101,20 @@ def fill_complex_fields(lang):
         slavic_dict = row[12]
         orig_srcs = row[14]
         citation = row[17]
+        add_philol = row[19]
+        add_histor = row[20]
         db.execute('''
                     INSERT INTO complex_fields
-                    (lemma, etymology, semantics, slavic_dict, orig_srcs, citation)
-                    VALUES (?, ?, ?, ?, ?, ?)
-                    ''', (lemma, etymology, semantics, slavic_dict, orig_srcs, citation))
+                    (lemma, etymology, semantics, slavic_dict, orig_srcs, citation, add_philol, add_histor, lang)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (lemma, etymology, semantics, slavic_dict, orig_srcs, citation, add_philol, add_histor, lang))
         conn.commit()
     print("complex data has been filled successfully!")
 
 
 def fill_sources(lang):
     print("filling sources...")
-    conn = sqlite3.connect(lang + '_dict.db')
+    conn = sqlite3.connect('GrPolDict.db')
     db = conn.cursor()
     for row in data_generator(lang):
         historic = row[11].split('; ')
@@ -120,38 +122,35 @@ def fill_sources(lang):
             if el != '':
                 db.execute('''
                 INSERT INTO sources
-                (lemma, source, type)
-                VALUES (?, ?, ?)
-               ''', (row[0], el, "hist"))
+                (lemma, source, type, lang)
+                VALUES (?, ?, ?, ?)
+               ''', (row[0], el, "hist", lang))
                 conn.commit()
         slavic = row[12].split('; ')
         for el in slavic:
             if el != '':
                 db.execute('''
                  INSERT INTO sources
-                (lemma, source, type)
-                VALUES (?, ?, ?)
-                ''', (row[0], el, "slavic"))
+                (lemma, source, type, lang)
+                VALUES (?, ?, ?, ?)
+                ''', (row[0], el, "slavic", lang))
                 conn.commit()
     print("sources have been filled successfully")
 
 
 def fill_variants(lang):
     print("filling variants...")
-    conn = sqlite3.connect(lang + '_dict.db')
+    conn = sqlite3.connect('GrPolDict.db')
     db = conn.cursor()
-    i = 0
     for row in data_generator(lang):
-        print(i)
-        i += 1
         phonetic = row[4].split(', ')
         for el in phonetic:
             if el != '':
                 db.execute('''
                 INSERT INTO variants
-                (lemma, var_type, variant)
-                VALUES (?, ?, ?)
-                ''', (row[0], "phon", el))
+                (lemma, var_type, variant, lang)
+                VALUES (?, ?, ?, ?)
+                ''', (row[0], "phon", el, lang))
                 conn.commit()
         morph = row[5].split('(')
         if morph[0] != '':
@@ -159,24 +158,37 @@ def fill_variants(lang):
             for var in vars:
                 db.execute('''
                 INSERT INTO variants
-                (lemma, var_type, variant)
-                VALUES (?, ?, ?)
-                ''', (row[0], "morph", var))
+                (lemma, var_type, variant, lang)
+                VALUES (?, ?, ?, ?)
+                ''', (row[0], "morph", var, lang))
                 conn.commit()
     print("variants have been filled successfully")
 
 
 def process_greek():
+    print("Starting greek processing...")
     lang = "greek"
-    makefile(lang)
     fill_simple_fields(lang)
     fill_complex_fields(lang)
     fill_sources(lang)
     fill_variants(lang)
+    print("greek has been processed!")
+
+
+def process_polish():
+    print("Starting polish processing...")
+    lang = "polish"
+    fill_simple_fields(lang)
+    fill_complex_fields(lang)
+    fill_sources(lang)
+    fill_variants(lang)
+    print("polish has been processed!")
 
 
 def main():
+    makefile()
     process_greek()
+    process_polish()
     return 0
 
 
